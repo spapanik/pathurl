50c0f78d0e3efce3015b637b576c418bbfbc1db5
Stephanos Kuma <stephanos@kuma.ai>
2025-06-24 12:36:27 +0100

    🔄 Use IntEnum for ports

--- src/pathurl/lib/constants.py
+++ src/pathurl/lib/constants.py
@@ -1,23 +1,23 @@
-from enum import Enum
+from enum import IntEnum
 
 
-class Port(Enum):
+class Port(IntEnum):
     ftp = 21
     sftp = 22
-    ssh = 22  # noqa: PIE796
+    ssh = 22
     telnet = 23
     dns = 53
     gopher = 70
     http = 80
-    ws = 80  # noqa: PIE796
+    ws = 80
     https = 443
-    wss = 443  # noqa: PIE796
+    wss = 443
     ipp = 631
-    ipps = 631  # noqa: PIE796
+    ipps = 631
     mariadb = 3306
-    mysql = 3306  # noqa: PIE796
+    mysql = 3306
     postgres = 5432
-    postgresql = 5432  # noqa: PIE796
+    postgresql = 5432
     redshift = 5439
     redis = 6379
     git = 9418
